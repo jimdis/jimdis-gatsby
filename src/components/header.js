8d70ba9b3c5494e5f4301f41ceadd0e303718429
Jim Disenstam <jim.disenstam@gmail.com>
2019-10-27 10:00:35 +0100

    Initial test site

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,35 @@
-import { Link } from "gatsby"
+import { Link as GatLink } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import "tachyons"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+const classes = {
+  header: "sans-serif",
+  navWrapper: "bg-black-80",
+  nav: "dt w-100 mw8 center",
+  homeWrapper: "dtc w2 v-mid pa3",
+  linksWrapper: "dtc v-mid tr pa3",
+  link: "f6 fw4 hover-white no-underline white-70 dn dib-ns pv2 ph3",
+}
+
+const Link = ({ to, children, rest }) => (
+  <GatLink to={to} className={classes.link} {...rest}>
+    {children}
+  </GatLink>
+)
+
+const Header = ({ siteTitle, links }) => (
+  <header className={classes.header}>
+    <div className={classes.navWrapper}>
+      <nav className={classes.nav}>
+        <div className={classes.homeWrapper}>
+          <Link to="/">{siteTitle}</Link>
+        </div>
+        <div className={classes.linksWrapper}>
+          <Link to="/">Music</Link>
+          <Link to="/">Portfolio</Link>
+        </div>
+      </nav>
     </div>
   </header>
 )
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,8 +7,9 @@ import SEO from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
+    <h1>Hi from the second page!!11</h1>
     <p>Welcome to page 2</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
